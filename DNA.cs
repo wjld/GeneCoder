4c39f52b193e1e7eca9848a2a8840d592821903f
wjld <98484634+wjld@users.noreply.github.com>
2022-10-25 08:25:14 -0300

    add dictionaries for bits and chars translations

---
+++ DNA.cs
@@ -0,0 +1,28 @@
+class DNA
+{
+    string strand;
+
+    static readonly Dictionary<string,char> bitsToChar = new Dictionary<string,char>
+    {
+        {"00",'A'},
+        {"01",'C'},
+        {"10",'G'},
+        {"11",'T'},
+    };
+
+    static readonly Dictionary<char,string> charToBits = new Dictionary<char,string>
+    {
+        {'A',"00"},
+        {'C',"01"},
+        {'G',"10"},
+        {'T',"11"},
+    };
+
+    static readonly Dictionary<char,char> complement = new Dictionary<char,char>
+    {
+        {'A','T'},
+        {'C','G'},
+        {'G','C'},
+        {'T','A'},
+    };
+}
