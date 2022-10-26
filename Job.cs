ba35a301d19bd742a8ba8824c2d73f535441c19b
wjld <98484634+wjld@users.noreply.github.com>
2022-10-26 15:59:48 -0300

    add job solving and answer instantiation

--- Job.cs
+++ Job.cs
@@ -14,16 +14,33 @@ public class Job
             case "DecodeStrand":
             {
                 this.urlParameters = $"{this.id}/decode";
+                DNA dna = new DNA(items["strandEncoded"]);
+                dna.Decode();
+                DecodeAnswer answer = new DecodeAnswer();
+                answer.strand = dna.GetStrand();
+                this.answer = answer;
                 break;
             }
             case "EncodeStrand":
             {
                 this.urlParameters = $"{this.id}/encode";
+                DNA dna = new DNA(items["strand"]);
+                dna.Encode();
+                EncodeAnswer answer = new EncodeAnswer();
+                answer.strandEncoded = dna.GetStrand();
+                this.answer = answer;
                 break;
             }
             case "CheckGene":
             {
                 this.urlParameters = $"{this.id}/gene";
+                DNA dna = new DNA(items["strandEncoded"]);
+                DNA gene = new DNA(items["geneEncoded"]);
+                dna.Decode();
+                gene.Decode();
+                GeneAnswer answer = new GeneAnswer();
+                answer.isActivated = dna.CheckGene(gene.GetStrand());
+                this.answer = answer;
                 break;
             }                
         }
