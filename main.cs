74e19b4f14fed9989d8f0a686953f9b278af5558
wjld <98484634+wjld@users.noreply.github.com>
2022-10-26 14:36:20 -0300

    add logging in routine

--- main.cs
+++ main.cs
@@ -6,6 +6,20 @@ class main
     static User user = new User();
     static Display display = new Display();
 
+    static async Task<string> RequestTokenAsync(User user)
+    {
+        string token = null;
+        HttpResponseMessage response = await client.PostAsJsonAsync(
+            "api/users/login", user);
+        var fields = await response.Content.
+            ReadAsAsync<Dictionary<string,string>>();
+        if (fields["code"] == "Success")
+        {
+            token = fields["accessToken"];
+        }
+        return token;
+    }
+
     static void Main()
     {
         RunAsync().GetAwaiter().GetResult();
@@ -19,6 +33,27 @@ class main
             new MediaTypeWithQualityHeaderValue("application/json"));
 
         string option = display.WelcomeScreen();
+        while(option != "3")
+        {
+            if(option == "1")
+            {
+                user.username = display.GetUsername();
+                user.password = display.GetPassword();
+            }
+            
+            string token = await RequestTokenAsync(user);
+            if(token is null)
+            {
+                display.LoginError();
+            }
+            else
+            {
+                display.ConfirmLogin(token);
+                client.DefaultRequestHeaders.Authorization = 
+                    new AuthenticationHeaderValue(token);
+            }
+            option = display.WelcomeScreen();
+        }
         Console.Clear();
     }
 }
